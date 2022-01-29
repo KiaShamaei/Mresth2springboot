78efc6986264adc9bf808fcd2ccd878054e8a4ae
Shamaei.Kiarash <kiarash.shamaii@gmail.com>
2022-01-29 19:20:51 +0330

    add crud

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,10 +4,11 @@ package com.example.demo.controller;
 import com.example.demo.model.Users;
 import com.example.demo.repository.UserRepo;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.tomcat.jni.User;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -21,5 +22,17 @@ public class UserController {
     @RequestMapping(path = "/users")
     public List getAllUsers (){
         return  userRepo.findAll();
+    }
+    @RequestMapping(path= "/user/{id}")
+    public Optional<Users> getUser(@PathVariable Integer id){
+        return userRepo.findById(id);
+    }
+    @RequestMapping(path = "/user/{id}" , method = RequestMethod.DELETE)
+    public void deleteUser(@PathVariable Integer id){
+        userRepo.deleteById(id);
+    }
+    @RequestMapping(path= "user" , method = RequestMethod.POST)
+    public Users editSaveUser (@RequestBody Users user){
+        return userRepo.save(user);
     }
 }
